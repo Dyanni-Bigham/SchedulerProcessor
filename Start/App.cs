5c5aa4135aae90c12f3a101d0004cf820fe26bb3
D'Yanni <dyanniham@gmail.com>
2024-05-27 11:45:22 -0700

    create App class

---
+++ Start/App.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace Start
+{
+    public class App
+    {
+        public static void Start()
+        {   
+            // Read the config file (maybe make this a class that can do that) (FileReader class)
+
+            // Extract the day, time, and application (file path) ignore the timeUnit field for now (FileReader class)
+
+            // have a flag called IsRunning this will be triggered by the context memu in the UI
+
+                // This will be a while loop that will constantly run the scheduling logic
+
+                // Create a class that will handle the processing (Processor class)
+
+            // Create an exeception class (Error class)
+            Console.WriteLine("Application is starting");
+        }
+    }
+}
---
+++ Start/Base.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Start
+{
+    public class Base
+    {
+        public static void Main(string[] args)
+        {
+            // Call the App class that will start the program
+            App.Start();
+            
+        }
+    }
+}
