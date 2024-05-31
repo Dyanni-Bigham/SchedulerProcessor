5294684257babb9fc549fc93cc12c1c90ea27ffb
D'Yanni <dyanniham@gmail.com>
2024-05-30 20:00:57 -0700

    ability to extract days, apps, and interval

--- Utils/FileReader.cs
+++ Utils/FileReader.cs
@@ -7,18 +7,31 @@ namespace Utils
 {
     class FileReader
     {
-        public void ReadFile(string path)
+        private string filePath;
+        private List<Entry> entries;
+        // Create a constructor where it takes in a filepath to the config file
+        public FileReader(string path)
         {
-            if (File.Exists(path))
+            this.filePath = path;
+            this.entries = new List<Entry>();
+        }
+        public void ReadFile()
+        {
+            if (File.Exists(this.filePath))
             {
-                string json = File.ReadAllText(path);
-                List<Entry> entries = JsonConvert.DeserializeObject<List<Entry>>(json);
+                string json = File.ReadAllText(this.filePath);
+                List<Entry> newEntries = JsonConvert.DeserializeObject<List<Entry>>(json);
 
-                foreach (Entry entry in entries)
+                foreach (Entry entry in newEntries)
                 {
-                    Console.WriteLine("Days: " + string.Join(", ", entry.Days ?? new List<string>()));
-                    Console.WriteLine("App: " + string.Join(", ", entry.Apps ?? new List<string>()));
-                    Console.WriteLine("Interval: " + entry.Interval);
+                    /*
+                    Console.WriteLine("Days: " + string.Join(", ", entry.days ?? new List<string>()));
+                    Console.WriteLine("App: " + string.Join(", ", entry.apps ?? new List<string>()));
+                    Console.WriteLine("Interval: " + entry.interval);
+                    */
+                    Entry newEntry = new Entry(entry.days, entry.apps, entry.interval);
+                    this.entries.Add(newEntry);
+                    Console.WriteLine("Entry successfully added");
 
                 }
             }
@@ -28,5 +41,18 @@ namespace Utils
             }
         }
         
+        public List<Entry> getEntries()
+        {
+            return this.entries;
+        }
+
+        public void displayEntries()
+        {
+            for (int i = 0; i < this.entries.Count; i++)
+            {
+                Console.WriteLine($"Element: {i}\n");
+                Console.WriteLine($"Entry: {this.entries[i].ToString()}");
+            }
+        }
     }
 }
--- obj/Debug/net8.0/SchedulerProcesser.AssemblyInfo.cs
+++ obj/Debug/net8.0/SchedulerProcesser.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchedulerProcesser")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c5aa4135aae90c12f3a101d0004cf820fe26bb3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d8561c44847475aadc88b38674c9f74184ce06e")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchedulerProcesser")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchedulerProcesser")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
