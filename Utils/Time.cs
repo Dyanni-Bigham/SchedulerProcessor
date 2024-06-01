985ef6cc211444b951cb323165357c894d3da311
D'Yanni <dyanniham@gmail.com>
2024-06-01 16:22:40 -0700

    add string helper utility

---
+++ Utils/Time.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Utils
+{
+    public class Time
+    {
+        public static void ConvertToSeconds(string timeToConvert)
+        {
+            //TODO
+        }
+    }
+}
--- obj/Debug/net8.0/SchedulerProcesser.AssemblyInfo.cs
+++ obj/Debug/net8.0/SchedulerProcesser.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchedulerProcesser")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d8561c44847475aadc88b38674c9f74184ce06e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d76388f60d95cee161b415d4980032e6925e24b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchedulerProcesser")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchedulerProcesser")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
