5f312e9479c736884d97b62485b4619856e6b8fb
Dyanni-Bigham <dyanniham@gmail.com>
2024-06-09 17:45:01 -0700

    able to find the interval in the schedule template

--- Utils/Processor.cs
+++ Utils/Processor.cs
@@ -3,32 +3,31 @@ using Objects;
 
 namespace Utils
 {
-    
+
     public class Processor
     {
         // Have global dictionary for the dictionary
         private static Dictionary<string, Dictionary<string, List<string>>> scheduleTemplate =
            new Dictionary<string, Dictionary<string, List<string>>>();
         private List<Entry> entries;
+        private static string currentDay;
 
         public Processor(List<Entry> entries)
         {
             this.entries = entries;
+            currentDay = DateTime.Now.DayOfWeek.ToString();
         }
-        public static void CreateDaySchedule()
+        public void CreateDaySchedule()
         {
             // Call create dictionary method
             Dictionary<string, Dictionary<string, List<string>>> test = CreateDayDictionary();
-            printSchedule();
-            
 
             // call add to dictionary method
+            AddEntryToDictionary(entries);
         }
 
         private static Dictionary<string, Dictionary<string, List<string>>> CreateDayDictionary()
         {
-            // get the current day of the week
-            string currentDay = DateTime.Now.DayOfWeek.ToString();
 
             string[] timeSlots = GenerateTimeSlots();
 
@@ -60,9 +59,33 @@ namespace Utils
             }
         }
 
-        private static void AddEntryToDictionary()
+        private static void AddEntryToDictionary(List<Entry> entries)
         {
-            //TODO Add Implementation
+            // Assuming the entries will share the same schedule
+            for (int i = 0; i < entries.Count; i++)
+            {
+                // check that entry day is the current day
+                if (entries[i].days.Contains(currentDay))
+                {
+                    // check that current day is in the schedule
+                    if(scheduleTemplate.ContainsKey(currentDay))
+                    {
+                        // gets all of the time slots for the current day
+                        var timeSlots = scheduleTemplate[currentDay];
+
+                        // find the time slot in the time slots dictionary
+                        if(timeSlots.ContainsKey(entries[i].interval))
+                        {
+                           Console.WriteLine(entries[i].interval);
+                        }
+                    }
+                }
+            }
+            // check that entry day is the current day
+
+            // search for entry interval in template
+
+            // assign that interval the entry app string
         }
 
         private static string[] GenerateTimeSlots()
@@ -76,7 +99,7 @@ namespace Utils
                     timeSlots.Add(string.Format("{0:D2}:{1:D2}", hour, minute));
                 }
             }
-            
+
             // return time slots
             return timeSlots.ToArray();
         }
