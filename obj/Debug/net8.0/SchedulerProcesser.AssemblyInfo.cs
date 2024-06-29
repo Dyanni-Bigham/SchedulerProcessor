fff03cffbd21e37efa579d33db4414fe8570c00e
D'Yanni <dyanniham@gmail.com>
2024-06-28 17:06:43 -0700

    add logging

--- obj/Debug/net8.0/SchedulerProcesser.AssemblyInfo.cs
+++ obj/Debug/net8.0/SchedulerProcesser.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchedulerProcesser")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f91307dab06c93765848d0df1e0cf524d5dd08c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2cae6357c6ff5547aee0db559539be865323ddd")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchedulerProcesser")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchedulerProcesser")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
